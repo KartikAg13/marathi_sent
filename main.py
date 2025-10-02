391192af65c67d4a6f71f0d5c4a2b7301cde9a26
KartikAg13 <23803013@mail.jiit.ac.in>
2025-10-02 17:01:08 +0530

    start with model

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from marathi-sent!")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+import tensorflow as tf
+from keras import Sequential, Model
+from keras.layers import Embedding, LSTM, Bidirectional, Dense, Dropout, Input
+from keras.utils import pad_sequences
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.optimizers import Adam
 
-if __name__ == "__main__":
-    main()
+from tokenizers import Tokenizer
+from sklearn.metrics import classification_report, confusion_matrix
